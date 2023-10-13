ad90f3a95de3b6d8406a2ac4d1441b1d9a91e3e0
Jose Romero <jgromerom89@gmail.com>
2023-10-13 11:10:54 -0300

    update

--- src/component/intro/Intro.jsx
+++ src/component/intro/Intro.jsx
@@ -6,25 +6,28 @@ import Social from '../Social/social'
 export default class Intro extends Component {
   render() {
     return (
-        <div className="mi-componente">
-        <div className="contenido">
-          <div>
-            <h2 className='intro'>Jose Gregorio Romero Mendoza</h2>
-            <h2 className='intro'>Full Stack Developers</h2>
-            <h2 className='intro'>MERS</h2>
-           <ul className='li'>M - MongoDB</ul>
-           <ul className='li'>E - ExpressJS</ul>
-           <ul className='li'>R - ReactJS</ul>
-           <ul className='li'>N - NodeJS</ul>
-            <Social/>
-          </div>
-        </div>
-        <div className="avatar">
-          <img className="imagen"
+      <div class="parent">
+<div class="div1">            
+          <img src='https://www.cyberzon3.com/img/React.png'className='res' alt='imagen1'/>
+           <img src='https://cdn.icon-icons.com/icons2/2415/PNG/512/mongodb_plain_wordmark_logo_icon_146423.png' className='res'alt='imagen2'/>
+</div>
+<div class="div2">  
+    <img className="avatar" 
             src={img}
             alt="Imagen"
-          />
-        </div>
+          /> 
+          <div><h2 className='title'>Jose Romero</h2></div>
+      </div>
+<div class="div3">           <img src='https://www.cyberzon3.com/img/express.png' className='res' alt='imagen3'/>
+           <img src='https://static-00.iconduck.com/assets.00/node-js-icon-1901x2048-mk1e13df.png' className='res'alt='imagen4'/>
+    </div>
+
+      
+          
+         {/* <Social/> */}
+            
+         
+        
       </div>
     )
   }
