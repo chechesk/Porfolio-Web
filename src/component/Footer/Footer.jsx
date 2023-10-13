96d13f00812a036bef2886f2cec59ab1c379ad0a
Jose Romero <jgromerom89@gmail.com>
2023-10-13 11:15:55 -0300

    update

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Social from "../Social/social";
 import './Footer.css'
 
 const Footer = () =>{
     return (
         <div className="Footer">
-    <h3>   JR devs @ 2023 Todos los Derechos Reservados </h3>
+    <h3>   JR devs @ 2023 Todos los Derechos Reservados </h3> <Social/>
     </div>
     )
 }
--- src/component/intro/Intro.jsx
+++ src/component/intro/Intro.jsx
@@ -1,33 +1,26 @@
 import React, { Component } from 'react'
 import img from '../../assets/1684808450050.jpeg'
 import './intro.css'
-import Social from '../Social/social'
 
 export default class Intro extends Component {
   render() {
     return (
       <div class="parent">
-<div class="div1">            
-          <img src='https://www.cyberzon3.com/img/React.png'className='res' alt='imagen1'/>
-           <img src='https://cdn.icon-icons.com/icons2/2415/PNG/512/mongodb_plain_wordmark_logo_icon_146423.png' className='res'alt='imagen2'/>
-</div>
-<div class="div2">  
-    <img className="avatar" 
-            src={img}
-            alt="Imagen"
-          /> 
+            <div class="div1">            
+                  <img src='https://www.cyberzon3.com/img/React.png'className='res' alt='imagen1'/>
+                  <img src='https://cdn.icon-icons.com/icons2/2415/PNG/512/mongodb_plain_wordmark_logo_icon_146423.png' className='res'alt='imagen2'/>
+            </div>
+      <div class="div2">  
+             <img className="avatar" 
+                    src={img}
+                    alt="Imagen"
+                  /> 
           <div><h2 className='title'>Jose Romero</h2></div>
       </div>
-<div class="div3">           <img src='https://www.cyberzon3.com/img/express.png' className='res' alt='imagen3'/>
+              <div class="div3">          
+           <img src='https://www.cyberzon3.com/img/express.png' className='res' alt='imagen3'/>
            <img src='https://static-00.iconduck.com/assets.00/node-js-icon-1901x2048-mk1e13df.png' className='res'alt='imagen4'/>
-    </div>
-
-      
-          
-         {/* <Social/> */}
-            
-         
-        
+         </div>
       </div>
     )
   }
