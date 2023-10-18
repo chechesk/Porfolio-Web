31b8ee47362354cff5335bebabafc623ddf19fdf
Jose Romero <jgromerom89@gmail.com>
2023-10-18 01:45:37 -0300

    head
    tittle

--- src/component/Config/Helmet.jsx
+++ src/component/Config/Helmet.jsx
@@ -1,5 +1,5 @@
 
-export const pageTitle = "Juan Pérez - Desarrollador Web Full Stack MERN";
+export const pageTitle = "Jose Romero - Desarrollador Web Full Stack MERN";
 export const pageDescription = "Soy un apasionado Desarrollador Web Full Stack con experiencia en tecnologías MERN. Mi objetivo es crear soluciones web innovadoras y de alta calidad que satisfagan las necesidades de mis clientes. ¡Explora mis proyectos y descubre cómo puedo ayudarte a lograr tus objetivos en desarrollo web!";
 export const keywords = [
     "Desarrollador Web Full Stack",
